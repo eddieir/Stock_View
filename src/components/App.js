c917fcbc0c55db61edfadcf4da28acabe3ad882a
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-04-23 17:58:24 +0200

    Final update - finished designed and finilized the charts and final render - ready for dev

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import StockContainer from '../container/StockContainer';
+import Search from '../container/Search';
+import WatchList from './WatchList';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Container fluid>
+          <Navbar hover="true" style={{ backgroundColor: "#3d4147" }} expand="lg" variant="dark" sticky="top">
+            <Navbar.Brand style={{ color: "#20c94d" }} href="/stock_view">stock_view</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/stock_view">Home</Nav.Link>
+                <Nav.Link href="/watchlist">Watchlist</Nav.Link>
+              </Nav>
+              <Search />
+            </Navbar.Collapse>
+          </Navbar>
+          <br />
+          <Route exact path="/stock_view" component={StockContainer} />
+          <Route exact path="/watchlist" component={WatchList} />
+        </Container>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,84 @@
+import {
+  SEARCH_STOCK,
+  FETCH_STOCK,
+  REQUEST_STOCK,
+  RECEIVE_STOCK,
+  RECEIVE_PROFILE
+} from '../constants';
+
+const initialState = {
+  symbol: '',
+  data: {
+    open: [],
+    high: [],
+    close: [],
+    low: []
+  },
+  volume: [],
+  isFetching: false,
+  resolution: '',
+  name: '',
+  description: '',
+  isDataLoaded: false
+}
+
+function generateData(finData) {
+  var data = [];
+  for (var i = 0; i < finData.c.length; i++) {
+    data.push([
+      finData.t[i] * 1000,
+      finData.o[i],
+      finData.h[i],
+      finData.l[i],
+      finData.c[i]
+    ]);
+  }
+  return (data);
+}
+
+function generateVolumeData(finData) {
+  var data = [];
+  for (var i = 0; i < finData.c.length; i++) {
+    data.push([
+      finData.t[i] * 1000,
+      finData.v[i]
+    ]);
+  }
+  return (data);
+}
+
+function rootReducar(state = initialState, action = FETCH_STOCK) {
+  switch (action.type) {
+    case SEARCH_STOCK:
+      return Object.assign({}, state, {
+        symbol: action.symbol
+      })
+    case REQUEST_STOCK:
+      return Object.assign({}, state, {
+        isFetching: true
+      })
+    case RECEIVE_STOCK:
+      if (action.data.s === "ok") {
+        return Object.assign({}, state, {
+          isFetching: false,
+          symbol: action.symbol,
+          data: generateData(action.data),
+          volume: generateVolumeData(action.data),
+          isDataLoaded: true
+        })
+      }
+      return state;
+    case RECEIVE_PROFILE:
+      if (action.name !== undefined) {
+        return Object.assign({}, state, {
+          name: action.name,
+          description: action.description
+        })
+      }
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default rootReducar;
