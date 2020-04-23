4dfdfc8edcf52a2737c9a8a8d80c8a4fdf3d388c
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-04-23 18:07:52 +0200

    update

---
+++ build/precache-manifest.ac2be9afe2efce4b7900d84f105b09c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce5397396357a1edca43fe5e2c96812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ebf25803bd5d32d5f2a",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "63fbb4b7f9c1b0a277ab",
+    "url": "/static/css/main.b1786856.chunk.css"
+  },
+  {
+    "revision": "7ebf25803bd5d32d5f2a",
+    "url": "/static/js/2.5be00fb5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5be00fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63fbb4b7f9c1b0a277ab",
+    "url": "/static/js/main.f541fa31.chunk.js"
+  },
+  {
+    "revision": "6e24e4a3836121007058",
+    "url": "/static/js/runtime-main.e4d48c03.js"
+  }
+]);
